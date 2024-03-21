77e7ae3feb3ea607121d144f0b850ebae874cc46
Md Ismail Hossain <shojeeb.aust@gmail.com>
2024-03-21 17:30:54 +0100

    Update middleware.py

--- edge/middleware.py
+++ edge/middleware.py
@@ -1,14 +1,23 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
 def process_data():
     data = request.get_json()  # Get the data from the request
+
+    response_from_fog = send_api_request_fog('http://192.168.10.147:5000/api', "edge")
+
+    print(response_from_fog)
+    
     # Process the data and generate a response
     response = {'message': 'Data received successfully', 'data': data}
     return jsonify(response)
 
+def send_api_request_fog(url, data):
+        response = requests.post(url, json=data)
+        return response.json()
 
 
 if __name__ == '__main__':
