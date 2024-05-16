cdd9b9a3e71d076e3ac7f7a215db75aab9eb3476
Md Ismail Hossain <shojeeb.aust@gmail.com>
2024-05-16 16:21:25 +0200

    Update middleware.py

--- new/edge/middleware.py
+++ new/edge/middleware.py
@@ -25,7 +25,8 @@ def handle_container_api_request():
         payload = {'message': res, 'task': 'task2'}
         response = requests.post('http://192.168.10.147:5000/api', json=payload)
         print(response.text)
-        response = requests.post('http://192.168.10.243:5003/api', json=response)
+        payload = {'message': "task 2 started", 'task': 'task2'}
+        response = requests.post('http://192.168.10.243:5003/api', json=payload)
         print(response.text)
         print("task2 is due")
     return {'result': 'Data received in middleware API'}
