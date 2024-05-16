bdc0834e8946833076bf539f28612b2f679ff0d1
Md Ismail Hossain <shojeeb.aust@gmail.com>
2024-05-16 16:11:55 +0200

    a

--- new/fog/middleware.py
+++ new/fog/middleware.py
@@ -12,35 +12,84 @@ app = Flask(__name__)
 def handle_api_request():
     request_data = request.get_json()
     global task_type
+    
     # Extract the request type from the request data
     task_type = request_data.get('task')
-    print(task_type)
-    print(request_data.get('message'))
+    
+    # print(task_type)
+    # print(request_data.get('message'))
 
     if task_type == 'task2' or task_type == 'task3':
         # Perform task 2
-        threading.Thread(target=perform_task2, args=(request_data,)).start()
-        result = {'result': 'Task 2 thread started, wait for result'} 
+        result = negotiate_fog()
+        print(result)   
+        if result == "success":
+            threading.Thread(target=perform_task2, args=(request_data,)).start()
+            return {'result': 'Task 2 deployed successfully wait for result'}
+        else:
+            return {'result': 'Task 2 failed because of fog negotiation failure'}
     else:
         # Invalid request type
-        result = {'error': 'Invalid request type'}
+        return {'error': 'Invalid request type'}
 
-    return result
     # return {'result': 'data received in fog middleware API'}
 
 def perform_task2(request_data):
     # Logic for task 1
     # ...
-    # result = negotiate_edge()
     print("inside thread")
-    print(request_data.get('message'))
-    # if result == "success":
-    #     # return {'result': 'Task 1 completed'}
-    #     deploy_pod()
-    #     # deploy_service(request_data.get('message'))
-    #     return {'result': 'Task 1 deployed successfully wait for result'}
-    # else:
-    #     return {'result': 'Task 1 failed because of edge negotiation failure'}
+    # print(request_data.get('message'))
+    
+
+def negotiate_fog():
+    script_path = "/root/thesis/new/edge/bash.sh" 
+    script_output = run_shell_script(script_path)
+    
+    # Split the string into lines
+    lines = script_output.strip().split('\n')
+
+    # Extract headers
+    headers = lines[0].split()
+
+    # Initialize dictionaries to store data for each node
+    node_data = {}
+
+    # Process data for each line
+    for line in lines[1:]:
+        # Split line into fields
+        fields = line.split()
+        node_name = fields[0]
+        node_values = {
+            headers[i]: fields[i] for i in range(1, len(headers))
+        }
+        node_data[node_name] = node_values
+    
+    negotiation = "unsuccess"
+    for node_name, values in node_data.items():
+        if int(values['CPU%'].rstrip('%')) < 50 and int(values['MEMORY%'].rstrip('%')) < 50:
+            print(f"Node: {node_name} -> CPU usage is {values['CPU%']} and memory usage is {values['MEMORY%']}")
+            negotiation = "success"
+        else:
+            print(f"Node: {node_name} -> CPU usage is {values['CPU%']} and memory usage is {values['MEMORY%']}")
+
+    # # negotiation with fog
+    # response_from_fog = send_api_request_fog('http://192.168.10.147:5000/api', "negotiate")
+    # print(response_from_fog)
+    # return jsonify({'reply': negotiation, 'data': node_data})
+    return negotiation
+
+def run_shell_script(script_path):
+    try:
+        # Run the shell script using subprocess and capture output
+        completed_process = subprocess.run(['bash', script_path], capture_output=True, text=True, check=True)
+        # Access the captured output from completed_process.stdout
+        script_output = completed_process.stdout
+        # Return the captured output
+        return script_output
+    except subprocess.CalledProcessError as e:
+        print(f"Error running shell script: {e}")
+        return None
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
