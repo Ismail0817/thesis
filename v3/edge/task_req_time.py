b3c0933448828f3d55e421d4da1e98c761578040
Md Ismail Hossain <shojeeb.aust@gmail.com>
2024-06-04 14:18:39 +0200

    Update task_req_time.py

--- v3/edge/task_req_time.py
+++ v3/edge/task_req_time.py
@@ -4,7 +4,7 @@ import requests
 url = 'http://192.168.10.145:5000/api'
 
 # Define the request payload
-payload = {'message': 'task3', 'time': 60}
+payload = {'message': 'task3', 'time': 180}
 
 # Send the POST request
 response = requests.post(url, json=payload)
